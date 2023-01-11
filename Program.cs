bdbb16f16a064e31f1aa781748ecaca3ee61d288
belar <bel.arth92@gmail.com>
2023-01-11 11:03:15 +0300

    Final commit

--- Program.cs
+++ Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿string [] array1 = new string [5] {"ad", "nauseam", "621", "inner", "start"};
+string [] array2 = new string [array1.Length];
+
+void Arrays(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+Arrays(array1, array2);
+PrintArray(array2);
